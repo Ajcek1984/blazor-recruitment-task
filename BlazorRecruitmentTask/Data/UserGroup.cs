d24e30f4f702d88bf015630775a2b4ad4d708d45
Jacek Salamon <ajcek84@gmail.com>
2024-03-16 13:45:55 +0100

    Lista grup

---
+++ BlazorRecruitmentTask/Data/UserGroup.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorRecruitmentTask.Data
+{
+    public class UserGroup
+    {
+        public required string Identifier { get;set; }
+
+        public required string Name { get;set; }
+
+
+    }
+}
