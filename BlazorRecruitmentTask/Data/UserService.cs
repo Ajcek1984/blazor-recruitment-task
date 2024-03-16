eb6cc9cc61fa2e4dedc6a7e37ce71a3a241f4f5f
Jacek Salamon <ajcek84@gmail.com>
2024-03-16 12:56:42 +0100

    Zasadniczo działający drag&drop

--- BlazorRecruitmentTask/Data/UserService.cs
+++ BlazorRecruitmentTask/Data/UserService.cs
@@ -8,10 +8,10 @@ namespace BlazorRecruitmentTask.Data
             new User {  Name = "Adam Kowalski", City = "Radom", Photo="images/user3.png", Group = UnasssignedUsersGroup },
             new User {  Name = "Piotr Piotrowski", City = "Radom", Photo="images/user2.png", Group = UnasssignedUsersGroup },
             new User {  Name = "Magda Nowakowska", City = "Warszawa", Photo="images/user1.png", Group = UnasssignedUsersGroup },
-            new User {  Name = "Jan Nowak", City = "Radom", Photo="images/user4.png", Group = UnasssignedUsersGroup }
+            new User {  Name = "Jan Nowak", City = "Radom", Photo="images/user4.png", Group = "Group1" }
 
         };
 
-        public Task<List<User>> GetUsersAsync() => Task.FromResult(Users.ToList());
+        public Task<User[]> GetUsersAsync() => Task.FromResult(Users);
     }
 }
